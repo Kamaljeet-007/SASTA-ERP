4abfc69211f3ff75dc498c3398627c1ee78d88f0
Kamaljeet-007 <kamaljeet006940@gmail.com>
2024-10-19 13:14:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SASTA-ERP
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Kamaljeet-007/SASTA-ERP)
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { createClient } = require('@supabase/supabase-js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+// Supabase configuration
+const SUPABASE_URL = 'https://wrrwcfcicjckpcgqyqau.supabase.co';
+const SUPABASE_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndycndjZmNpY2pja3BjZ3F5cWF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY4MzE2MDYsImV4cCI6MjA0MjQwNzYwNn0.2U3ysbRwSIJOHE5DjagUkb0QFmCF3-0fIlgyckBrZio';
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+// Endpoint for fetching all rows
+app.post('/query', async (req, res) => {
+  const { table } = req.body;
+  try {
+    const { data, error } = await supabase.from(table).select('*'); // Fetch all rows
+
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+
+    res.json({ data });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: 'An error occurred while executing the query' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
